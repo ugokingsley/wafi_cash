647364d7088c81c1cb8825341c21254c2c8d2f99
ugokingsley <ugokingsley5@gmail.com>
2022-09-14 16:14:10 +0100

    create multi-currency wallet

--- wallet_test.py
+++ wallet_test.py
@@ -7,7 +7,7 @@ class WafiAccountTest(unittest.TestCase):
         a = WafiAccount('a', {"USD": 10, "NGN" : 20, "GBP":10, "YUAN": 10})
         b = WafiAccount('b', {"USD": 0, "NGN" : 0, "GBP":0, "YUAN": 0})
         a.transfer("USD", 5, b)
-        self.assertEqual(b.account_statement(), {"USD": 5, "NGN" : 20, "GBP":10, "YUAN": 10})
+        self.assertEqual(b.account_statement(), {"USD": 5, "NGN" : 0, "GBP":0, "YUAN": 0})
         #self.assertEqual(a.account_statement(), {"USD": 5, "NGN" : 20, "GBP":10, "YUAN": 10})
 
 
